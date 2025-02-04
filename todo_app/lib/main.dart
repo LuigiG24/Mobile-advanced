1fbd2770f579099d3dc31327842a7ea45c96ce4f
Luigi Garcia <luigi2410@hotmail.com>
2025-02-03 20:28:09 -0800

    Firebase main dart

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/auth/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(const ToDoApp());
 }
 
