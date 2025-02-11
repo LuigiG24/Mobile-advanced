ecb806b121ad2ae90bf1bb0f7e078e52f028d286
Luigi Garcia <luigi2410@hotmail.com>
2025-02-10 21:23:16 -0800

    l

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:todo_app/pages/add_item_page.dart';
 import 'package:todo_app/pages/home.dart';
 
 void main() async {
